eda82736bafabc2fb0adcfb8a334112ce550859f
RizqiTatsumaki <44367829+RizqiTatsumaki@users.noreply.github.com>
2019-04-03 14:42:00 +0700

    Create install-bahan.sh

---
+++ install-bahan.sh
@@ -0,0 +1,24 @@
+clear
+echo "Sedang Menginstall Semua Bahan Yang Di Perlukan :)"
+sleep 2
+apt-get update && upgrade
+sleep 2
+apt-get install python
+sleep 2
+apt-get install python2
+sleep 2
+pip2 install requirements
+sleep 2
+pip2 install html2text
+sleep 2
+pip2 install requests
+sleep 2
+pip2 install nmap
+sleep 2
+pip2 install mechanize
+clear
+echo "Sudah Di Install Gan, Tunggu 2 Detik Akan Kembali"
+echo " "
+echo " "
+echo "python2 home.py"
+echo ""
